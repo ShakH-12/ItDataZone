1634e71ef73e46707cea13a1958b6326585fcaf6
ShakH-12 <shakh7887@gmail.com>
2026-03-25 19:12:26 +0500

    init

--- ItDataZone/apps/main/views.py
+++ ItDataZone/apps/main/views.py
@@ -5,6 +5,5 @@ from django.views import View
 
 class HomeView(View):
     permission_classes = [permissions.AllowAny]
-
     def get(self, request):
         return render(request, 'main/home.html')
